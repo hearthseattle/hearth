bd4aa6f92dc6f523701089493fb5bff615edff32
bonanashelby <bonanashelby@gmail.com>
2017-07-24 11:59:02 -0700

    Casey and I are working on bootstrap

--- searchlist/searchlist/settings.py
+++ searchlist/searchlist/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'searchlist'
-    'search'
+    'searchlist',
+    'search',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
