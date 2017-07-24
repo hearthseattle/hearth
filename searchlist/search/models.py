c82148a42a3ebfdfe6ba6d93e27564a04c00519f
Ophelia Yin <owyowy1@gmail.com>
2017-07-24 12:10:18 -0700

    search models views

--- searchlist/search/models.py
+++ searchlist/search/models.py
@@ -1,3 +1,91 @@
-from django.db import models
+"""Model for search profiles."""
 
-# Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.encoding import python_2_unicode_compatible
+from taggit.managers import TaggableManager
+
+
+MAIN_CATEGORY = (
+    ("center", "center")
+    ("shelter", "shelter"),
+    ("food", "food"),
+    ("clinic", "clinic")
+)
+
+RATINGS = (
+    ("one badge", "one badge"),
+    ("two badge", "two badge"),
+    ("three badge", "three badge")
+)
+
+
+class ProfileManager(models.Manager):
+    """Manage search profiles."""
+
+    def get_queryset(self):
+        """Return all active searches."""
+        return super(ProfileManager, self).get_queryset().filter(user__is_active=True).all()
+
+
+@python_2_unicode_compatible
+class SearchProfile(models.Model):
+    """A profile for organization in our application."""
+
+    org = models.OneToOneField(
+        User,
+        related_name="profile",
+        on_delete=models.CASCADE
+    )
+    main_category = models.CharField(
+        max_length=25,
+        choices=MAIN_CATEGORY,
+        default="center",
+        null=True
+    )
+    ratings = models.CharField(
+        max_length=25,
+        choices=RATINGS,
+        default="color",
+        null=True
+    )
+
+    location = models.CharField(max_length=255, blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+
+    objects = models.Manager()
+    active = ProfileManager()
+    tags = TaggableManager()
+
+
+    @property
+    def is_active(self):
+        """Active status."""
+        return self.user.is_active
+
+    def __repr__(self):
+        """Print username."""
+        return """
+        org_name: {}
+        main_category: {}
+        ratings: {}
+        location: {}
+        website: {}
+        """.format(self.org.username, self.main_category, self.ratings, self.location, self.website)
+
+    def __str__(self):
+        """Print organization name."""
+        return self.__repr__()
+
+
+@receiver(post_save, sender=User)
+def make_profile_for_new_org(sender, **kwargs):
+    """New Profile instances."""
+    if kwargs['created']:
+        new_profile = SearchProfile(
+            user=kwargs['instance']
+        )
+        new_profile.save()
