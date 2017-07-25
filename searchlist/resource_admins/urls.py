21e2cd69a86ac787bee457fb637f7c83f60eafc2
Kurt <kurtrm@gmail.com>
2017-07-25 14:11:36 -0700

    Added edit, delete, update views.

---
+++ searchlist/resource_admins/urls.py
@@ -0,0 +1,8 @@
+"""Urls related to the creating and editing resources."""
+from django.conf.urls import url
+from views import CreateResource, EditResource
+
+urlpatterns = [
+    url(r'^new/$', CreateResource.as_view(), name='create_resource'),
+    url(r'^edit/$', EditResource.as_view(), name='edit_resource')
+]
