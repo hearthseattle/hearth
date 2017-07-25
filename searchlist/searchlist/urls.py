869b89e8c05e082d818499e5752b281cb08bf133
Kurt <kurtrm@gmail.com>
2017-07-25 15:22:05 -0700

    Attempting login/logout stuff.

--- searchlist/searchlist/urls.py
+++ searchlist/searchlist/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from searchlist.views import HomePageView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,6 +24,14 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomePageView.as_view(), name='home'),
-    url(r'^search/', include("search.urls", namespace="search", app_name="search")),
-    url(r'^resource/', include("resource_admins.urls"))
+    url(r'^search/', include("search.urls",
+                             namespace="search",
+                             app_name="search")),
+    url(r'^resource/', include("resource_admins.urls")),
+    url(r'^login/$', auth_views.LoginView.as_view(
+        template_name='registration/login.html'),
+        name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(
+        template_name='searchlist/home.html'),
+        name='logout')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
