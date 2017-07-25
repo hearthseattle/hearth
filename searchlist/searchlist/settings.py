1dff70c69273ae1e7a6615c848dd732bb2cb9e16
Miguel Pena <miguelp1986@gmail.com>
2017-07-25 09:41:37 -0700

    changed settings file

--- searchlist/searchlist/settings.py
+++ searchlist/searchlist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-52-89-210-22.us-west-2.compute.amazonaws.com', 'localhost', '127.0.0.1']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
