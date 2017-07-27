53c3dde1cc262235d2dae03b5da81c02c7a40c26
Ophelia Yin <owyowy1@gmail.com>
2017-07-27 09:31:01 -0700

    test 3

--- searchlist/searchlist/tests.py
+++ searchlist/searchlist/tests.py
@@ -6,7 +6,6 @@ from django.test import TestCase, Client, RequestFactory
 from django.contrib.auth.models import User
 from searchlist.models import Resource
 from searchlist.views import CreateResource, EditResource, DeleteResource, HomePageView, ResourceDetailView
-from bs4 import BeautifulSoup
 import factory
 import faker
 import os
@@ -36,6 +35,7 @@ AGE_RANGE = (
 
 HERE = os.path.dirname(__file__)
 
+
 ############ MODEL TESTS
 class ResourceFactory(factory.django.DjangoModelFactory):
     """Factory for creating resources."""
@@ -52,7 +52,6 @@ class ResourceFactory(factory.django.DjangoModelFactory):
     description = fake.text(254)
     ratings = random.choice(RATINGS)[0]
     age_range = random.choice(AGE_RANGE)[0]
-    location = fake.address()
     website = fake.domain_name()
     org_name = fake.name()
     phone_number = factory.Sequence(lambda n: '123-555-%04d' % n)
@@ -80,7 +79,6 @@ class ResourceTestModels(TestCase):
         test_org.description = 'test case'
         test_org.age_range = '18-25'
         test_org.ratings = 'two badge'
-        test_org.location = 'seattle'
         test_org.website = 'http://www.test_org.com'
         test_org.phone_number = '1234567890'
         test_org.tags = 'shelter'
@@ -90,7 +88,6 @@ class ResourceTestModels(TestCase):
         self.assertTrue(test_org.description == 'test case')
         self.assertTrue(test_org.age_range == '18-25')
         self.assertTrue(test_org.ratings == 'two badge')
-        self.assertTrue(test_org.location == 'seattle')
         self.assertTrue(test_org.website == 'http://www.test_org.com')
         self.assertTrue(test_org.phone_number == '1234567890')
         self.assertTrue(test_org.tags == 'shelter')
@@ -123,13 +120,20 @@ class ResourceTestModels(TestCase):
         self.assertTrue(Resource.objects.count(), 6)
 
 
-############ ROUTE, TEMPLATE TESTS
+# ############ ROUTE, TEMPLATE TESTS
 class ViewRouteTest(TestCase):
     """."""
 
     def setUp(self):
         self.client = Client()
         self.request = RequestFactory()
+        self.resource = ResourceFactory.build()
+        self.resource.save()
+
+        test_fred = User()
+        test_fred.username = 'fred'
+        test_fred.set_password('temporary')
+        test_fred.save()
 
     def test_homepage_view(self):
         """Reverse home route to test link exist."""
@@ -144,120 +148,117 @@ class ViewRouteTest(TestCase):
 
     def test_404_view(self):
         """Test not exist resource 404 page appears."""
-        response = self.client.get('foo')
+        response = self.client.get('/resource/200')
         self.assertEqual(response.status_code, 404)
-        # self.assertTemplateUsed(response, 'searchlist/404.html')
 
     def test_createresource_view(self):
         """Test reverse creatersource route to test link exists."""
-        response = self.client.get(reverse_lazy('/resource/new/'))
+        self.client.login(username='fred', password='temporary')
+        response = self.client.get('/resource/new/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'searchlist/resource_form.html')
 
-    def test_Editresource_view(self):
+    def test_editresource_view(self):
         """Test reverse editsource route to test link exists."""
-        response = self.client.get(reverse_lazy('/resource/1/edit/'))
+        self.client.login(username='fred', password='temporary')
+        idx = self.resource.id
+        response = self.client.get("/resource/{}/edit/".format(idx))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'searchlist/resource_form.html')
 
-    def test_Deleteresource_view(self):
+    def test_deleteresource_view(self):
         """Test reverse deletesource route to test link exists."""
-        response = self.client.get(reverse_lazy('/resource/1/delete/'))
+        self.client.login(username='fred', password='temporary')
+        idx = self.resource.id
+        response = self.client.get('/resource/{}/delete/'.format(idx))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'searchlist/delete_resource.html')
 
-    def test_Resourcedetail_view(self):
+    def test_resourcedetail_view(self):
         """Test reverse resourcedetail route to test link exists."""
-        response = self.client.get(reverse_lazy('/resource/1'))
+        idx = self.resource.id
+        response = self.client.get('/resource/{}'.format(idx))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'searchlist/resource_detail.html')
 
-    def test_Login_view(self):
+    def test_login_view(self):
         """Test reverse login route to test link exists."""
-        response = self.client.get(reverse('/login/'))
+        response = self.client.get('/login/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'registration/login.html')
 
 
-############ View TESTS
+# ############ View TESTS
 
-class RegistrationTest(TestCase):
+class RegistrationCreateEditDeleteResourceTest(TestCase):
     """."""
 
     def setUp(self):
         """."""
         self.client = Client()
+        test_fred = User()
+        test_fred.username = 'fred'
+        test_fred.set_password('temporary')
+        test_fred.save()
+        self.resource = ResourceFactory.build()
+        self.resource.save()
 
     def test_valid_user_login(self):
-        """Test login process."""
-        user = User(username='fred', email='test@test.com')
-        user.set_password('temporary')
-        user.save()
+        """Test login process with good crendentials."""
         self.client.login(username='fred', password='temporary')
         response = self.client.get('/login/', follow=True)
         self.assertTrue(response.context['user'].is_active)
 
     def test_invalid_user_login_username(self):
-        """Test login process."""
-        user = User(username='fred', email='test@test.com')
-        user.set_password('temporary')
-        user.save()
+        """Test login process with bad credentials."""
         self.client.login(username='bill', password='temporary')
         response = self.client.get('/login/', follow=True)
         self.assertFalse(response.context['user'].is_authenticated)
 
     def test_valid_user_logout(self):
         """Test logout process."""
-        user = User(username='fred', email='test@test.com')
-        user.set_password('temporary')
-        user.save()
         self.client.login(username='fred', password='temporary')
         response = self.client.get('/logout/')
         self.assertEqual(response.status_code, 302)
 
+    def test_homepageview(self):
+        """Test homepage."""
+        response = self.client.get(reverse_lazy('home'))
+        import pdb; pdb.set_trace()
 
-# class CreateResourceTest(TestCase):
-#     """Create a resource."""
+
+# class DeleteResourceTest(TestCase):
+#     """Delete a resource."""
 
 #     def setUp(self):
-#         """."""
+#         """Create a resource."""
 #         self.client = Client()
+#         self.resource = ResourceFactory.build()
+#         self.resource.save()
 
-#     def test_CreateResourceView()
+#     def test_Deleteresource_cancel_button(self):
+#         """Test cancel button."""
+#         self.assertEqual(Resource.objects.count(), 1)
+#         idx = self.resource.id
+#         response = self.client.get('/resource/{}/delete/'.format(idx))
+#         html = BeautifulSoup(response.content, 'Cancel')
+#         self.assertEqual(response.status_code, 302)
+#         self.assertEqual(Resource.objects.count(), 1)
 
+#     def test_Deleteresource_confirm_button(self):
+#         """Test confirm button."""
+#         self.assertEqual(Resource.objects.count(), 1)
+#         idx = self.resource.id
+#         self.client.post('/resource/{}/delete/'.format(idx))
+#         self.assertEqual(Resource.objects.count(), 0)
+#         self.assertEqual(response.status_code, 302)
 
-class DeleteResourceTest(TestCase):
-    """Delete a resource."""
+#     # class EditResourceTest(TestCase):
+#     # """Edit a resource."""
 
-    def setUp(self):
-        """Create a resource."""
-        self.client = Client()
-        self.resource = ResourceFactory.build()
-        self.resource.save()
+#     # def setUp(self):
+#     #     """."""
+#     #     self.client = Client()
 
-    def test_Deleteresource_cancel_button(self):
-        """Test cancel button."""
-        self.assertEqual(Resource.objects.count(), 1)
-        idx = self.resource.id
-        response = self.client.get('/resource/{}/delete/'.format(idx))
-        html = BeautifulSoup(response.content, 'Cancel')
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(Resource.objects.count(), 1)
-
-    def test_Deleteresource_confirm_button(self):
-        """Test confirm button."""
-        self.assertEqual(Resource.objects.count(), 1)
-        idx = self.resource.id
-        self.client.post('/resource/{}/delete/'.format(idx))
-        self.assertEqual(Resource.objects.count(), 0)
-        self.assertEqual(response.status_code, 302)
-
-    # class EditResourceTest(TestCase):
-    # """Edit a resource."""
-
-    # def setUp(self):
-    #     """."""
-    #     self.client = Client()
-
-    # def test_edit
+#     # def test_edit
 
