5423c1ba275eda72503884a7286c415717bdd27b
Miguel Pena <miguelp1986@gmail.com>
2017-07-26 18:07:26 -0700

    changes to settings file

--- searchlist/searchlist/settings.py
+++ searchlist/searchlist/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 LOGIN_REDIRECT_URL = 'home'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), '/var/www/static/')# Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), '/var/www/static/')# Extra places for collectstatic to find static files.
 
 if not DEBUG:
     AWS_STORAGE_BUCKET_NAME = 'homeless-to-hearth'
