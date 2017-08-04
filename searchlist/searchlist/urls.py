8cda08da84b15a6794ee54f1d7a40d722de1bb2c
Kurt <kurtrm@gmail.com>
2017-08-04 11:11:17 -0700

    Fooled around with custom 404. Learned a little bit.

--- searchlist/searchlist/urls.py
+++ searchlist/searchlist/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
-from django.conf.urls import url, include
+from django.conf.urls import (
+    url,
+    include,
+    handler404
+)
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from rest_framework import routers
@@ -30,6 +34,8 @@ from searchlist.views import (
 
 router = routers.DefaultRouter()
 router.register(r'resources', ResourceViewSet)
+
+handler404 = 'searchlist.views.NotFound'
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
